c265e34090a274c4ec86ad2c181b55ac14dd6de5
Oliver Degener <oliver.degener@monday-consulting.com>
2023-03-29 17:53:18 +0200

    Support exploded (layered) Spring Boot apps
    Exploded apps are detected via the BOOT-INF/layers.idx file and launched with Spring Boot's JarLauncher. The launcher can be customized with the env variable SPRING_BOOT_EXPLODED_MAINCLASS.
    
    Also introduces useful default JVM args and the new EXTRA_JAVA_OPTS env variable which is concatenated to JAVA_OPTS.

--- temurin/11/run.sh
+++ temurin/11/run.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-exec java ${JAVA_OPTS} -jar app.jar ${@}
+set -e
+
+if [ -n "${EXTRA_JAVA_OPTS}" ]; then
+  JAVA_OPTS="$JAVA_OPTS $EXTRA_JAVA_OPTS"
+fi
+
+JAVA_CMD_ARGS="-XX:-UsePerfData \
+               ${JAVA_OPTS} \
+               -XX:+UseContainerSupport \
+               -Djava.security.egd=file:/dev/./urandom"
+
+if [ -f BOOT-INF/layers.idx ]; then
+  exec java $JAVA_CMD_ARGS ${SPRING_BOOT_EXPLODED_MAINCLASS:-org.springframework.boot.loader.JarLauncher} ${@}
+else
+  exec java $JAVA_CMD_ARGS -jar app.jar ${@}
+fi
