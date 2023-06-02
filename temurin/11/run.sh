e8fc82b3831fe253fc312d361ff841dce708c248
Oliver Degener <oliver.degener@monday-consulting.com>
2023-06-02 09:46:58 +0200

    Support Jib images
    Jib does not use Spring Boot's layer mechanism. Instead, a jib-classpath-file and a jib-main-class-file are written which can be used to launch the application.

--- temurin/11/run.sh
+++ temurin/11/run.sh
@@ -10,7 +10,9 @@ JAVA_CMD_ARGS="-XX:-UsePerfData \
                -XX:+UseContainerSupport \
                -Djava.security.egd=file:/dev/./urandom"
 
-if [ -f BOOT-INF/layers.idx ]; then
+if [ -f jib-classpath-file -a -f jib-main-class-file ]; then
+  exec java $JAVA_CMD_ARGS -cp @jib-classpath-file @jib-main-class-file
+elif [ -f BOOT-INF/layers.idx ]; then
   exec java $JAVA_CMD_ARGS ${SPRING_BOOT_EXPLODED_MAINCLASS:-org.springframework.boot.loader.JarLauncher} ${@}
 else
   exec java $JAVA_CMD_ARGS -jar app.jar ${@}
