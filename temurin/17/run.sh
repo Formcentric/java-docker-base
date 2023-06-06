0f193b20d9bdc056b9b578c4647864b031b16935
Oliver Degener <oliver.degener@monday-consulting.com>
2023-06-06 08:29:33 +0200

    Jib: Pass container command args to Java

--- temurin/17/run.sh
+++ temurin/17/run.sh
@@ -11,7 +11,7 @@ JAVA_CMD_ARGS="-XX:-UsePerfData \
                -Djava.security.egd=file:/dev/./urandom"
 
 if [ -f jib-classpath-file -a -f jib-main-class-file ]; then
-  exec java $JAVA_CMD_ARGS -cp @jib-classpath-file @jib-main-class-file
+  exec java $JAVA_CMD_ARGS -cp @jib-classpath-file @jib-main-class-file ${@}
 elif [ -f BOOT-INF/layers.idx ]; then
   exec java $JAVA_CMD_ARGS ${SPRING_BOOT_EXPLODED_MAINCLASS:-org.springframework.boot.loader.JarLauncher} ${@}
 else
